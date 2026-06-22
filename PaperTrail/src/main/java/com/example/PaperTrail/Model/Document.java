dea6b0b32566fad7339e0c0c698e902fae720780
rohit <rohitrohilla8930@gmail.com>
2026-06-23 03:32:48 +0530

    Adding Entity Classes

---
+++ PaperTrail/src/main/java/com/example/PaperTrail/Model/Document.java
@@ -0,0 +1,42 @@
+package com.example.PaperTrail.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Document {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false, name = "owner_id")
+    private User user;
+
+    @Column(name ="original_fileName", nullable = false)
+    private String originalFileName;
+
+    @Column(nullable = false, name = "storage_alias", unique = true)
+    private String storageAlias;
+
+    @Column(nullable = false, name = "encryption_iv")
+    private String encryptionIv;
+
+    @Column(name="file-size", nullable = false)
+    private Long fileSize;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false,updatable = false)
+    private LocalDateTime createdAt;
+
+}
